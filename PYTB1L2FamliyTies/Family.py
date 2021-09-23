87300a6d773160e2b70cae9651ef85e31e36f139
mo <mosawer.asg@outlook.com>
2021-09-23 11:59:18 +0200

    Family Ties

---
+++ PYTB1L2FamliyTies/Family.py
@@ -0,0 +1,6 @@
+naam = "Mosawer"
+leeftijd = "20"
+woonplaats = "Hoofddorp"
+afstand = "14"
+print("Mijn naam is " + naam + ", ik ben " + leeftijd + " jaar oud en woon in " + woonplaats + ".")
+print("Ik woon", afstand,"kilometer van het Mediacollege.")
