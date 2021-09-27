ba084ba067f80c70be3e510ac2cd7128e8b2a258
mo <mosawer.asg@outlook.com>
2021-09-27 12:12:14 +0200

    schooltool

---
+++ PYTB1L3SchoolTool/SchoolTool.py
@@ -0,0 +1,9 @@
+print("Welke dag is het vandaag?")
+huidigeDag = input()
+
+if huidigeDag == "Maandag" or huidigeDag == "Dinsdag" or huidigeDag == "Woensdag" or huidigeDag == "Donderdag" or huidigeDag == "Vrijdag":
+    print("Pak je spullen en ga er heen!")
+elif huidigeDag == "maandag" or huidigeDag == "dinsdag" or huidigeDag == "woensdag" or huidigeDag == "donderdag" or huidigeDag == "vrijdag":
+    print("Pak je spullen en ga er heen!")
+else:
+    print("Lekker uitslapen!")
