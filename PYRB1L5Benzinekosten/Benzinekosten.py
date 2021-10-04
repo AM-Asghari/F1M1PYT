ee62813b1419b617014e4c624e260bde900d6414
mo <mosawer.asg@outlook.com>
2021-10-04 12:26:34 +0200

    return statement

--- PYRB1L5Benzinekosten/Benzinekosten.py
+++ PYRB1L5Benzinekosten/Benzinekosten.py
@@ -3,5 +3,9 @@ benzine_kosten_per_liter = 1.54
 liter_per_kilometer = 0.2 
 km_per_maand = input("Hoeveel kilometer rij jij per maand? ")
 km_per_maand = float(km_per_maand)
-maandkosten = (km_per_maand * liter_per_kilometer * benzine_kosten_per_liter) + verzekering_per_maand
-print("Jij betaald:", str(maandkosten) + " Euro per maand!")
+
+def maandelijkse_kosten(_km_per_maand):
+    return (_km_per_maand * liter_per_kilometer * benzine_kosten_per_liter) + verzekering_per_maand
+
+print("Jij betaald:", str(maandelijkse_kosten(km_per_maand)) + " Euro per maand!")
+
